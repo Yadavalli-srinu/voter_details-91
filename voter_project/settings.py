6bc52e44edc18687820f3eff9c767039ba3bd8d9
Yadavalli srinu <yadavallis9@gmail.com>
2025-11-19 01:10:34 +0530

    added Procfile and requirements

--- voter_project/settings.py
+++ voter_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7r#y#1%t$$kco)!r=c&jy$d(e=(*1bd98#o4)2sy!)b+)$xx_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
